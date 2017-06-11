3034d7a435f0f70ae9bf0e0ecda05da6d05cdcc7
witold <w.mormul@gmail.com>
2017-06-11 10:36:34 +0200

    ActionBarWork

--- app/src/main/java/com/example/witold/android4/ActionBarActivity.java
+++ app/src/main/java/com/example/witold/android4/ActionBarActivity.java
@@ -1,11 +1,12 @@
 package com.example.witold.android4;
 
 import android.app.ActionBar;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Window;
 
-public class ActionBarActivity extends AppCompatActivity implements ActionBar.TabListener {
+public class ActionBarActivity extends Activity implements ActionBar.TabListener {
 
     Fragment11 f11;
     Fragment12 f12;
@@ -15,28 +16,26 @@ public class ActionBarActivity extends AppCompatActivity implements ActionBar.Ta
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_action_bar);
 
         f11 = new Fragment11();
         f12 = new Fragment12();
         f13 = new Fragment13();
-        transakcja = getSupportFragmentManager().beginTransaction();
-        transakcja.add(R.id.kontener, f11);
+        transakcja = getFragmentManager().beginTransaction();
+        transakcja.add(R.id.kontener2, f11);
         transakcja.detach(f11);
-        transakcja.add(R.id.kontener, f12);
+        transakcja.add(R.id.kontener2, f12);
         transakcja.detach(f12);
-        transakcja.add(R.id.kontener, f13);
+        transakcja.add(R.id.kontener2, f13);
         transakcja.detach(f13);
 
         ActionBar myAB = getActionBar();
         myAB.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        ActionBar.Tab myT = myAB.newTab();
-        myT.setText("tab1").setTabListener(this);
-        myAB.addTab(myT);
-        myT = myAB.newTab().setTabListener(this).setText("tab2");
-        myAB.addTab(myT);
-        myT = myAB.newTab().setTabListener(this).setText("tab3");
+        myAB.addTab(myAB.newTab().setText("tab1").setTabListener(this));
+        myAB.addTab(myAB.newTab().setText("tab2").setTabListener(this));
+        myAB.addTab(myAB.newTab().setText("tab3").setTabListener(this));
     }
 
     @Override
@@ -44,8 +43,6 @@ public class ActionBarActivity extends AppCompatActivity implements ActionBar.Ta
         int tabNumber = tab.getPosition();
         switch (tabNumber) {
             case 1:
-                transakcja.detach(f12);
-                transakcja.detach(f13);
                 transakcja.attach(f11);
                 break;
             case 2:
@@ -63,7 +60,18 @@ public class ActionBarActivity extends AppCompatActivity implements ActionBar.Ta
 
     @Override
     public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
-
+        int tabNumber = tab.getPosition();
+        switch (tabNumber) {
+            case 1:
+                transakcja.detach(f11);
+                break;
+            case 2:
+                transakcja.detach(f12);
+                break;
+            case 3:
+                transakcja.detach(f13);
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/example/witold/android4/Fragment1.java
+++ app/src/main/java/com/example/witold/android4/Fragment1.java
@@ -4,7 +4,7 @@ package com.example.witold.android4;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
